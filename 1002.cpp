af2767e28712beb6a9a9c5827249dad6e76a7eaa
JakubGajos <s25368@pja.edu.pl>
2021-10-17 16:14:51 +0200

    initial

---
+++ 1002.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+auto  dodawanie(int liczba,int a,int b)
+{
+	std::cout<<"podaj liczbe a";
+	std::cin>>a;
+	std::cout<<"podaj liczbe b";
+	std::cin>>b;
+	liczba=a+b;
+	return liczba;
+}
+int main()
+{
+	(dodawanie(liczba));
+	return 0;
+}
