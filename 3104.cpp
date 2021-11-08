651b21914fe52a479961057b70d830dddb8e5701
JakubGajos <s25368@pja.edu.pl>
2021-11-08 20:34:51 +0100

    8th commit

---
+++ 3104.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int suma(int n)
+{
+	int A[n];
+	int s=0;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"podaj A["<<i<<"]:";cin>>A[i];
+		s=s+A[i];
+	}
+	cout<<"suma:"; return s;	
+}
+
+int main ()
+{
+	int n;
+	cout<<"podaj liczbe elemntow tablicy A: ";cin>>n;
+	cout<<suma(n)<<endl;
+	return 0;
+}
