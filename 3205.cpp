afe01684b188ddc0a0995b8f37ff2f0b180024f2
JakubGajos <s25368@pja.edu.pl>
2021-11-19 01:42:11 +0100

    9th commit

--- 3205.cpp
+++ 3205.cpp
@@ -3,49 +3,29 @@
 
 using namespace std;
 
-vector<int> A;
+int A[64];
+int B[64];
 
 void insert_table(int n)
 {
-	for(int i=0;i<64;i++)
-	{
-	A.push_back(rand() % (n+1)+0);
-	}
+	int p;
+	
 }
 
-void quick_sort()
-{
-	int p=A[A.size()/2];
-	int i=0;
-	int j=A.size()-1;
-	do
-	{
-		if(A[i]>A[j])
-		{
-			int x=A[j];
-			A[j]=A[i];
-			A[i]=x;
-		}
-	i++;
-	j--;
-	}
-	while(A[i]!=p || A[j]!=p);
+void quick_sort(int A[],int B[])
+{	
 	
 }
 void show_table()
 {
-	for(int i=0;i<A.size();i++)
-	{
-		cout<<A[i]<<" ";
-	}
-	cout<<"\n";
+	
 }
 int main()
 {
 	int n;
 	cout<<"podaj zakres liczb losowych: ";cin>>n;
 	insert_table(n);
-	quick_sort();
+	quick_sort(A,B);
 	show_table();
 	return 0;
 }
