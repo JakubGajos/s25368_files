30dccca3558f7bd250ef59e0750adb9dfed83d13
JakubGajos <s25368@pja.edu.pl>
2022-01-21 14:48:23 +0100

    13th commit

--- 3403.cpp
+++ 3403.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int number_a=4;
-int number_b=6;
-int *a=&number_a;
-int *b=&number_b;
-
-void swap(int a, int b)
+int swap(int a, int b)
 {
 	int c=b;
 	b=a;
 	a=c;
-	cout<<"\n"<<"liczba a:"<<a<<"\n"<<"liczba b:"<<b<<"\n";
+	return a;
 }
 
 int main()
 {
+	int number_a=4;
+	int number_b=6;
+	cout<<"podaj a i b: ";cin>>number_a;cin>>number_b;
+	int *a=&number_a;
+	int *b=&number_b;
 	cout<<"przed zmianą:"<<"\n"<<"liczba a:"<<*a<<"\n"<<"liczba b:"<<*b<<"\n";
-	cout<<"po zmianie:";
-	swap(*a,*b);
+	cout<<"po zmianie:"<<swap(*a,*b)<<" "<<swap(*b,*a);
 	return 0;
 }
--- 3404.cpp
+++ 3404.cpp
@@ -3,19 +3,24 @@
 using namespace std;
 const double pi=3.14159265359;
 
-void cone(int h, int r)
+double cone(int h, int r, int a)
 {
 	double f=pi*pow(2,r);
 	double v=(f/3)*h;
-	cout<<"objętość wynosi: "<<v<<"\n"<<"pole podstawy wynosi: "<<f<<"\n";
+	if(a==0)
+	{
+	return f;
+	a++;
+	}
+	else return v;
 }
 
 int main()
 {
-	int h_value, r_value;
+	int h_value, r_value, a;
 	cout<<"podaj wysokość i promień stożka:"; cin>>h_value>>r_value;
 	int *h=&h_value;
 	int *r=&r_value;
-	cone(*h,*r);
+	cout<<"objętość wynosi: "<<cone(*h,*r,0)<<"\n"<<"pole podstawy wynosi: "<<cone(*h,*r,1)<<"\n";
 	return 0;
 }
